7648650ff39034c39acd140a6df2a1e2a41260d6
gokism <gokul.dynamite@gmail.com>
2015-05-06 16:04:53 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "Cloud Openshift Lab Final\n"+
+  "Cloud Openshift Lab Fina!!!l\n"+
 # ENV values are generated during template processing
 # and then passed to the container when openshift launches it.
   "User is #{ENV['ADMIN_USERNAME']}\n"+
